bc6984ebdd576a7d39766b3441e3b44de8e770a1
Prasadini V.G.H. <prasadinivgh.21@uom.lk>
2024-09-16 00:13:45 +0530

    search bar
    complete searchbar in homepage

--- app/home/index.js
+++ app/home/index.js
@@ -1,43 +1,113 @@
-import { FontAwesome6 } from '@expo/vector-icons';
-import React from 'react'
-import { View, Text, Pressable,StyleSheet } from 'react-native'
-import { useSafeAreaInsets } from 'react-native-safe-area-context'
-import { theme } from '../../constants/theme';
-import { hp, wp } from '../../helpers/common';
+import { Feather, FontAwesome6, Ionicons } from "@expo/vector-icons";
+import React, { useRef, useState } from "react";
+import {
+  View,
+  Text,
+  Pressable,
+  StyleSheet,
+  ScrollView,
+  TextInput,
+} from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { theme } from "../../constants/theme";
+import { hp, wp } from "../../helpers/common";
+import Categories from "../../components/categories";
 
 function HomeScreen() {
-    const {top} = useSafeAreaInsets();
-    const paddingTop = top>0 ? top+10 : 30;
+  const { top } = useSafeAreaInsets();
+  const paddingTop = top > 0 ? top + 10 : 30;
+  const [search, setSearch] = useState("");
+  const searchInputRef = useRef(null);
   return (
-    <View style={[styles.container, {paddingTop}]}>
-        <View style={styles.header}>
-            <Pressable>
-                <Text style={styles.title}>PixelVault</Text>
+    <View style={[styles.container, { paddingTop }]}>
+      <View style={styles.header}>
+        <Pressable>
+          <Text style={styles.title}>PixelVault</Text>
+        </Pressable>
+        <Pressable>
+          <FontAwesome6
+            name="bars-staggered"
+            size={22}
+            color={theme.colors.neutral(0.7)}
+          />
+        </Pressable>
+      </View>
+      <ScrollView contentContainerStyle={{ gap: 15 }}>
+        <View style={styles.searchBar}>
+          <View style={styles.searchIcon}>
+            <Feather
+              name="search"
+              size={24}
+              color={theme.colors.neutral(0.4)}
+            />
+          </View>
+          <TextInput
+            placeholder="Search for photos..."
+            value={search}
+            ref={searchInputRef}
+            onChangeText={(value) => setSearch(value)}
+            style={styles.searchInput}
+          />
+          {search && (
+            <Pressable style={styles.closeIcon}>
+              <Ionicons
+                name="close"
+                size={24}
+                color={theme.colors.neutral(0.6)}
+              />
             </Pressable>
-            <Pressable>
-                <FontAwesome6 name="bars-staggered" size={22} color={theme.colors.neutral(0.7)}/>
-            </Pressable>
+          )}
         </View>
+        <View style={styles.categories}>
+          <Categories/>
+        </View>
+      </ScrollView>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        gap:15
-    },
-    header:{
-        marginHorizontal: wp(4),
-        flexDirection:'row',
-        justifyContent:'space-between',
-        alignItems:'center',
-    },
-    title:{
-        fontSize: hp(4),
-        fontWeight:theme.fontweights.semibold,
-        color:theme.colors.neutral(0.9),
-    }
-})
+  container: {
+    flex: 1,
+    gap: 15,
+  },
+  header: {
+    marginHorizontal: wp(4),
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: hp(4),
+    fontWeight: theme.fontweights.semibold,
+    color: theme.colors.neutral(0.9),
+  },
+  searchBar: {
+    marginHorizontal: wp(4),
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderWidth: 1,
+    borderColor: theme.colors.grayBG,
+    backgroundColor: theme.colors.white,
+    padding: 6,
+    paddingLeft: 10,
+    borderRadius: theme.radius.lg,
+  },
+  searchIcon: {
+    padding: 8,
+  },
+  searchInput: {
+    flex: 1,
+    borderRadius: theme.radius.sm,
+    paddingVertical: 10,
+    fontSize: hp(1.8),
+  },
+  closeIcon: {
+    backgroundColor: theme.colors.neutral(0.1),
+    padding: 8,
+    borderRadius: theme.radius.sm,
+  },
+});
 
-export default HomeScreen
+export default HomeScreen;
