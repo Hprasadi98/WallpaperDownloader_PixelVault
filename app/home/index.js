845e23eb4ad94ca7c079bf05c8c542202aa7ed86
Prasadini V.G.H <prasadinivgh.21@uom.lk>
2024-09-15 23:34:15 +0530

    feat: header
    homescreen header complete

--- app/home/index.js
+++ app/home/index.js
@@ -1,12 +1,43 @@
+import { FontAwesome6 } from '@expo/vector-icons';
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Pressable,StyleSheet } from 'react-native'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import { theme } from '../../constants/theme';
+import { hp, wp } from '../../helpers/common';
 
 function HomeScreen() {
+    const {top} = useSafeAreaInsets();
+    const paddingTop = top>0 ? top+10 : 30;
   return (
-    <View>
-    <Text>HomeScreen</Text>
+    <View style={[styles.container, {paddingTop}]}>
+        <View style={styles.header}>
+            <Pressable>
+                <Text style={styles.title}>PixelVault</Text>
+            </Pressable>
+            <Pressable>
+                <FontAwesome6 name="bars-staggered" size={22} color={theme.colors.neutral(0.7)}/>
+            </Pressable>
+        </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        gap:15
+    },
+    header:{
+        marginHorizontal: wp(4),
+        flexDirection:'row',
+        justifyContent:'space-between',
+        alignItems:'center',
+    },
+    title:{
+        fontSize: hp(4),
+        fontWeight:theme.fontweights.semibold,
+        color:theme.colors.neutral(0.9),
+    }
+})
 
 export default HomeScreen
