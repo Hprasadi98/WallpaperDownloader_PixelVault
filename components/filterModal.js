51eccd639b2b2b1987fb80d66b1766a25c39d8ec
Prasadini V.G.H <prasadinivgh.21@uom.lk>
2024-09-17 15:27:11 +0530

    feat: backdrop
    add animated backdrop for filter modal

--- components/filterModal.js
+++ components/filterModal.js
@@ -1,6 +1,8 @@
 import { View, Text, StyleSheet } from 'react-native'
 import React, { useMemo } from 'react'
 import { BottomSheetModal, BottomSheetView } from '@gorhom/bottom-sheet'
+import { BlurView } from 'expo-blur'
+import Animated, { Extrapolation, interpolate, useAnimatedStyle } from 'react-native-reanimated'
 
 const FiltersModal = ({modalRef}) => {
     const snapPoints = useMemo(()=>['75%'],[]);
@@ -11,6 +13,7 @@ const FiltersModal = ({modalRef}) => {
         index={0}
         snapPoints={snapPoints}
         enablePanDownToClose={true}
+        backdropComponent={customBackdrop}
     >
         <BottomSheetView style={styles.contentContainer}>
             <Text>Awesom 😍</Text>
@@ -19,8 +22,40 @@ const FiltersModal = ({modalRef}) => {
   )
 }
 
-const styles = StyleSheet.create({
+const customBackdrop =({animatedIndex, style})=>{
+  const containerAnimatedStyle = useAnimatedStyle(()=>{
+    let opacity = interpolate(
+      animatedIndex.value,
+      [-1, 0], [0, 1],
+      Extrapolation.CLAMP
+    )
+    return {opacity}
+  })
+  const containerStyle = [
+    StyleSheet.absoluteFill,
+    style,
+    styles.overlay,
+    containerAnimatedStyle
+  ]
+  return(
+    <Animated.View style={containerStyle}>
+      <BlurView
+        style={StyleSheet.absoluteFill}
+        tint='dark'
+        intensity={50}
+        />
+    </Animated.View>
+  )
+}
 
+const styles = StyleSheet.create({
+  contentContainer:{
+    flex:1,
+    alignItems:'center',
+  },
+  overlay:{
+    backgroundColor: 'rgba(0,0,0,0.5'
+  }
 })
 
 export default FiltersModal
